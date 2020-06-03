89d5e94aa118fbda466f3b7c32b8f10b247afc5f
Glitch (trail-leather-hyena) <none>
2020-06-03 01:32:28 +0000

    🎌🗺 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+var multer  = require('multer')
+var bodyParser=require('body-parser')
+var upload = multer({ dest: './public' })
+// require and use "multer"...
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+app.post('/api/fileanalyse',upload.single('upfile'),(req,res,next)=>{
+    res.json({name:req.file.originalname, type:req.file.mimetype, size:req.file.size})
+  })
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
